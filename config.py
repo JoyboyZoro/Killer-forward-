0f4415ccc49853fd799edbd5b7531675d1d85fba
JoyboyZoro <154298406+JoyboyZoro@users.noreply.github.com>
2024-02-11 23:26:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "6435225"))
     API_HASH = environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6774817827:AAEoLh3IktJ6gpDywo_0X80V6i45PWZJwZ0") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6916847586:AAGzhQhihHIoKJFw3wdpqh4izLw5PRRDF48") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://heroku123456abc:wcadzyz8s6@cluster0.ngkzc2f.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
