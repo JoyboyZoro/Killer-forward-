e865d4d1b1c2391b01b3a0cecd64f20566192ffe
JoyboyZoro <154298406+JoyboyZoro@users.noreply.github.com>
2024-02-18 09:44:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,11 +1,12 @@
 import os
 import sys
-import asyncio 
+import asyncio
+import pyrogram.types
 from database import db
 from config import Config, temp
 from translation import Translation
 from pyrogram import Client, filters
-from pyrogram.types import
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument
 
 main_buttons = [[
         InlineKeyboardButton('📜 Support Group', url='https://t.me/HP_Bot_discuss_group'),
